3e600d7d6efbaf4371ff90e673fa3a8a1fd6b642
Belén Albeza <belen@hey.com>
2025-07-14 18:03:54 +0200

    :wrench: Project setup with Vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <>
+      <h1>👩‍🎤 SFX-8</h1>
+      <main className={styles.container}></main>
+    </>
+  );
+}
+
+export default App;
