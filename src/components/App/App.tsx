faeb10bd76dfb1721241939792a83ecaece79e64
Belén Albeza <belen@hey.com>
2025-07-15 11:40:15 +0200

    :wrench: Tweak file structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,12 @@
 import styles from "./App.module.css";
+import { Pacman } from "iconoir-react";
 
 function App() {
   return (
     <>
-      <h1>👩‍🎤 SFX-8</h1>
+      <h1 className={styles.title}>
+        <Pacman width={24} /> SFX-8
+      </h1>
       <main className={styles.container}></main>
     </>
   );
