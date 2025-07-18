74a1acaff8394ae4df1c968cff3b4cbacc1acf7f
Belén Albeza <belen@hey.com>
2025-07-18 11:10:33 +0200

    :sparkles: Flash message when share url is copied to the clipboard

--- src/components/FileControls/ShareButton.tsx
+++ src/components/FileControls/ShareButton.tsx
@@ -1,22 +1,37 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ShareAndroid } from "iconoir-react";
 
 import Button from "../ui/Button";
 import SfxContext from "../../context/sfx-context";
+import styles from "./ShareButton.module.css";
 
 export default function ShareButton() {
   const { serialize } = useContext(SfxContext);
+  const [showFlashMessage, setShowFlashMessage] = useState(false);
 
   const handleShareClick = () => {
     const sfx = serialize();
     const url = `${window.location.origin}?sfx=${sfx}`;
     navigator.clipboard.writeText(url);
-    console.log("Copied to clipboard:", url);
+
+    setShowFlashMessage(true);
+    // Hide flash message after 2 seconds
+    setTimeout(() => {
+      setShowFlashMessage(false);
+    }, 2000);
   };
 
   return (
-    <Button icon={ShareAndroid} onClick={handleShareClick}>
-      Share
-    </Button>
+    <>
+      <span
+        className={styles.flashMessage}
+        style={{ display: showFlashMessage ? "block" : "none" }}
+      >
+        URL copied to clipboard!
+      </span>
+      <Button icon={ShareAndroid} onClick={handleShareClick}>
+        Share
+      </Button>
+    </>
   );
 }
