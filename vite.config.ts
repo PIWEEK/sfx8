a7d0f30ebf6e45a4cea66f08518b58fc8f257bdf
Belén Albeza <belen@hey.com>
2025-07-18 12:40:45 +0200

    :wrench: Set up automatic deployments

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/sfx8/", // GitHub Pages base path
   css: {
     modules: {
       localsConvention: "camelCase",
